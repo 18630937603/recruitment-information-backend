db52f71b467add81a020abcb7366f90cb0cc7100
lzh's desktop pc <18630937603@163.com>
2022-05-24 13:07:20 +0800

    login 5

--- index.js
+++ index.js
@@ -51,17 +51,21 @@ router.get("/", async (ctx) => {
 router.post("/api/login", async (ctx) => {
   const { request } = ctx;
   const { code } = request.body;
-  try {
-    const result = await axios.get(`https://api.weixin.qq.com/sns/jscode2session?appid=${AppId}&secret=${AppSecret}&js_code=${code}&grant_type=authorization_code`)
-    console.log(result.openid)
-    ctx.body = {
-      code: 0,
-      data: result.openid,
-      msg: `${result.openid}已登录成功！`,
-    }
-  } catch (e) {
-    console.log(e)
-  }
+
+  console.log(code)
+  axios.get(`https://api.weixin.qq.com/sns/jscode2session?appid=${AppId}&secret=${AppSecret}&js_code=${code}&grant_type=authorization_code`).then(res=>{
+    console.log('成功')
+    console.log(res)
+    // console.log(result.openid)
+    // ctx.body = {
+    //   code: 0,
+    //   data: result.openid,
+    //   msg: `${result.openid}已登录成功！`,
+    // }
+  }).catch(err=>{
+    console.log('错误')
+    console.log(err)
+  })
 })
 
 // 小程序调用，获取微信 Open ID
