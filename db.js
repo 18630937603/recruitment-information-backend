c6b9c9ba5f82de0a0697379c8b34cff0a2af1b81
lzh's desktop pc <18630937603@163.com>
2022-05-31 09:43:16 +0800

    login api finished

--- db.js
+++ db.js
@@ -1,32 +1,80 @@
-const { Sequelize, DataTypes } = require("sequelize");
+const {Sequelize, DataTypes} = require("sequelize");
 
 // 从环境变量中读取数据库配置
-const { MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_ADDRESS = "" } = process.env;
+const {MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_ADDRESS = ""} = process.env;
 
 const [host, port] = MYSQL_ADDRESS.split(":");
 
 const sequelize = new Sequelize("nodejs_demo", MYSQL_USERNAME, MYSQL_PASSWORD, {
-  host,
-  port,
-  dialect: "mysql" /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */,
+    host,
+    port,
+    dialect: "mysql" /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */,
 });
 
-// 定义数据模型
-const Counter = sequelize.define("Counter", {
-  count: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    defaultValue: 1,
-  },
-});
+
+// 用户
+const User = sequelize.define("User", {
+    nickname: DataTypes.STRING, // 微信昵称
+    avatarURL: DataTypes.STRING, // 头像URL
+    // currentType: DataTypes.INTEGER, // 0为求职者，1为招聘者
+    wx_openid: {
+        type: DataTypes.STRING,
+        unique: true
+    },
+})
+
+// 工作
+const Job = sequelize.define("Job", {
+    jobName: DataTypes.STRING, // 职位名称
+    minSalary: DataTypes.INTEGER, // 最低工资，人民币
+    maxSalary: DataTypes.INTEGER, // 最高工资，人民币
+    city: DataTypes.STRING, // 所处城市
+    companyName: DataTypes.STRING, // 所属公司名称，必填
+    address: DataTypes.STRING, // 工作地详细地址
+    educationalRequirements: DataTypes.STRING, // 学历要求
+    jobRequirements: DataTypes.STRING, // 岗位要求
+    jobIntroduction: DataTypes.STRING, // 岗位简介
+    contact: DataTypes.STRING // 联系方式
+})
+
+// 求职意愿
+const Intention = sequelize.define("Intention", {
+    preferredJobType: DataTypes.STRING, // 期望的工作类型
+    salaryExpectation: DataTypes.INTEGER,
+    cityExpectation: DataTypes.STRING,
+    description: DataTypes.STRING, // 描述
+    bindResume: DataTypes.BOOLEAN // 是否绑定简历
+})
+
+// 简历
+const Resume = sequelize.define("Resume", {
+    name: DataTypes.STRING, // 真实姓名
+    sex: DataTypes.INTEGER, // 0为男，1为女
+    selfIntroduction: DataTypes.STRING, // 自我介绍
+    educationalBackground: DataTypes.STRING, // 学历(本科、硕士等)
+    graduatedFrom: DataTypes.STRING, // 毕业院校名称
+    portraitURL: DataTypes.STRING, // 个人照片URL
+    fileURL: DataTypes.STRING // 附件简历URL
+})
+
+User.hasMany(Job)
+User.hasMany(Intention)
+User.hasOne(Resume)
+Job.belongsTo(User)
+Intention.belongsTo(User)
+Resume.belongsTo(User)
+
 
 // 数据库初始化方法
 async function init() {
-  await Counter.sync({ alter: true });
+    await sequelize.sync({force: true})
 }
 
 // 导出初始化方法和模型
 module.exports = {
-  init,
-  Counter,
+    init,
+    User,
+    Job,
+    Intention,
+    Resume
 };
--- index.js
+++ index.js
@@ -4,21 +4,80 @@ const logger = require("koa-logger");
 const bodyParser = require("koa-bodyparser");
 const fs = require("fs");
 const path = require("path");
-const { init: initDB, Counter } = require("./db");
+const { init: initDB, User } = require("./db");
 const axios = require("axios");
+const {nanoid} = require('nanoid')
 
 const router = new Router();
 
-const homePage = fs.readFileSync(path.join(__dirname, "index.html"), "utf-8");
-
 const AppId = "wx874904c38445429a"
-// todo:hide this secret
-const AppSecret = "a9a95c739e05d9212eb9977ab3146082"
 
-// 首页
-router.get("/", async (ctx) => {
-  ctx.body = homePage;
+
+// 登录和自动注册
+router.get("/api/login", async (ctx) => {
+  // 获取微信 Open ID
+  if (ctx.request.headers["x-wx-source"]) {
+    const openid = ctx.request.headers["x-wx-openid"]
+    const user = await User.findOne({
+      where: {
+        wx_openid: openid
+      }
+    })
+    if(user===null) {  // 如果是第一次登录则自动注册
+      await User.create({
+        nickname: "微信用户" + nanoid(),
+        avatarURL: "https://thirdwx.qlogo.cn/mmopen/vi_32/POgEwh4mIHO4nibH0KlMECNjjGxQUq24ZEaGT4poC6icRiccVGKSyXwibcPq4BWmiaIGuG1icwxaQX6grC9VemZoJ8rg/132",
+        wx_openid: openid
+      })
+      console.log("用户"+openid+"已注册")
+      ctx.body = {
+        code: 0,
+        msg: `${openid}已注册并登录成功！`
+      };
+    }else {
+      ctx.body = {
+        code: 0,
+        msg: `${openid}已登录成功！`,
+        data: {
+          nickname: user.nickname,
+          avatarURL: user.avatarURL,
+        }
+      }
+    }
+  }
 });
+
+router.post("/api/jobsList",async ctx => {
+
+})
+
+router.post("/api/intentionsList",async ctx => {
+
+})
+
+router.post("/api/addJob",async ctx => {
+
+})
+
+router.post("/api/addIntention",async ctx => {
+
+})
+
+const app = new Koa();
+app
+  .use(logger())
+  .use(bodyParser())
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+const port = process.env.PORT || 80;
+async function bootstrap() {
+  await initDB();
+  app.listen(port, () => {
+    console.log("启动成功", port);
+  });
+}
+bootstrap();
 
 // 更新计数
 // router.post("/api/count", async (ctx) => {
@@ -38,28 +97,3 @@ router.get("/", async (ctx) => {
 //   };
 // });
 
-
-
-// 小程序调用，获取微信 Open ID
-router.get("/api/wx_openid", async (ctx) => {
-  if (ctx.request.headers["x-wx-source"]) {
-    console.log("openid:", ctx.request.headers["x-wx-openid"])
-    ctx.body = ctx.request.headers["x-wx-openid"];
-  }
-});
-
-const app = new Koa();
-app
-  .use(logger())
-  .use(bodyParser())
-  .use(router.routes())
-  .use(router.allowedMethods());
-
-const port = process.env.PORT || 80;
-async function bootstrap() {
-  await initDB();
-  app.listen(port, () => {
-    console.log("启动成功", port);
-  });
-}
-bootstrap();
